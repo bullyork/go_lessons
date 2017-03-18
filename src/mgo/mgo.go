dbf666e8f37ea75e7c90c32c51d4a8600bcafbde
york <york@SindeMacBook-Pro.local>
2017-03-18 09:14:16 +0800

    mgo

---
+++ src/mgo/mgo.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Person struct {
+	Name  string
+	Phone string
+}
+
+func main() {
+	session, err := mgo.Dial("mongodb://127.0.0.1:27017")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+	c := session.DB("test").C("people")
+	err = c.Insert(&Person{"Ale", "+55 53 8116 9639"}, &Person{"Cla", "+55 53 8402 8510"})
+	if err != nil {
+		panic(err)
+	}
+	result := Person{}
+	err = c.Find(bson.M{"name": "Ale"}).One(&result)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Phone:", result.Phone)
+}
