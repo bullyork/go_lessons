55997427ce3f176e75bf41612ca2beb43962f9e6
york <york@SindeMacBook-Pro.local>
2017-03-19 17:46:40 +0800

    channelandweb

---
+++ src/photoweb/photoweb.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+const (
+	UPLOAD_DIR = "./uploads"
+)
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		io.WriteString(w, "<form method=\"POST\" action=\"/upload\" "+
+			" enctype=\"multipart/form-data\">"+
+			"Choose an image to upload: <input name=\"image\" type=\"file\" />"+
+			"<input type=\"submit\" value=\"Upload\" />"+
+			"</form>")
+		return
+	}
+	if r.Method == "POST" {
+		f, h, err := r.FormFile("image")
+		if err != nil {
+			http.Error(w, err.Error(),
+				http.StatusInternalServerError)
+			return
+		}
+		filename := h.Filename
+		defer f.Close()
+		t, err := os.Create(UPLOAD_DIR + "/" + filename)
+		if err != nil {
+			http.Error(w, err.Error(),
+				http.StatusInternalServerError)
+			return
+		}
+		defer t.Close()
+		if _, err := io.Copy(t, f); err != nil {
+			http.Error(w, err.Error(),
+				http.StatusInternalServerError)
+			return
+		}
+		http.Redirect(w, r, "/view?id="+filename, http.StatusFound)
+	}
+}
+
+func main() {
+	http.HandleFunc("upload", uploadHandler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err.Error())
+	}
+}
