70537acfd72601f0b0312af4776fcc6c8a516f33
york <york@192.168.55.120>
2017-03-19 23:28:46 +0800

    实现相册功能

--- src/photoweb/photoweb.go
+++ src/photoweb/photoweb.go
@@ -2,53 +2,125 @@ package main
 
 import (
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"path"
+	"runtime/debug"
+	"text/template"
 )
 
 const (
-	UPLOAD_DIR = "./uploads"
+	UPLOAD_DIR   = "./uploads"
+	TEMPLATE_DIR = "./views"
 )
+
+var templates = make(map[string]*template.Template)
+
+func init() {
+	fileInfoArr, err := ioutil.ReadDir(TEMPLATE_DIR)
+	check(err)
+	var templateName, templatePath string
+	for _, fileInfo := range fileInfoArr {
+		templateName = fileInfo.Name()
+		if ext := path.Ext(templateName); ext != ".html" {
+			continue
+		}
+		templatePath = TEMPLATE_DIR + "/" + templateName
+		log.Println("Loading template:", templatePath)
+		t := template.Must(template.ParseFiles(templatePath))
+		templates[templateName] = t
+	}
+}
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
-		io.WriteString(w, "<form method=\"POST\" action=\"/upload\" "+
-			" enctype=\"multipart/form-data\">"+
-			"Choose an image to upload: <input name=\"image\" type=\"file\" />"+
-			"<input type=\"submit\" value=\"Upload\" />"+
-			"</form>")
-		return
+		renderHtml(w, "upload", nil)
 	}
 	if r.Method == "POST" {
 		f, h, err := r.FormFile("image")
-		if err != nil {
-			http.Error(w, err.Error(),
-				http.StatusInternalServerError)
-			return
-		}
+		check(err)
 		filename := h.Filename
 		defer f.Close()
 		t, err := os.Create(UPLOAD_DIR + "/" + filename)
-		if err != nil {
-			http.Error(w, err.Error(),
-				http.StatusInternalServerError)
-			return
-		}
+		check(err)
 		defer t.Close()
-		if _, err := io.Copy(t, f); err != nil {
-			http.Error(w, err.Error(),
-				http.StatusInternalServerError)
-			return
-		}
+		_, err = io.Copy(t, f)
+		check(err)
 		http.Redirect(w, r, "/view?id="+filename, http.StatusFound)
 	}
 }
 
-func main() {
-	http.HandleFunc("upload", uploadHandler)
-	err := http.ListenAndServe(":8080", nil)
+func isExists(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	return os.IsExist(err)
+}
+
+func listHandler(w http.ResponseWriter, r *http.Request) {
+	fileInfoArr, err := ioutil.ReadDir("./uploads")
+	check(err)
+	locals := make(map[string]interface{})
+	images := []string{}
+	for _, fileInfo := range fileInfoArr {
+		images = append(images, fileInfo.Name())
+	}
+	locals["images"] = images
+	renderHtml(w, "list", locals)
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	imageId := r.FormValue("id")
+	imagePath := UPLOAD_DIR + "/" + imageId
+	if exists := isExists(imagePath); !exists {
+		http.NotFound(w, r)
+		return
+	}
+	w.Header().Set("Content-Type", "image")
+	http.ServeFile(w, r, imagePath)
+}
+
+func renderHtml(w http.ResponseWriter, tmpl string, locals map[string]interface{}) {
+	tmpl += ".html"
+	err := templates[tmpl].Execute(w, locals)
+	check(err)
+}
+
+func check(err error) {
 	if err != nil {
-		log.Fatal("ListenAndServe:", err.Error())
+		panic(err)
+	}
+}
+
+func safeHandler(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if e, ok := recover().(error); ok {
+				http.Error(w, e.Error(), http.StatusInternalServerError)
+
+				// 或者输出自定义的 50x 错误页面
+				// w.WriteHeader(http.StatusInternalServerError)
+				// renderHtml(w, "error", e.Error())
+
+				// logging
+				log.Println("WARN: panic fired in %v.panic - %v", fn, e)
+				log.Println(string(debug.Stack()))
+			}
+		}()
+		fn(w, r)
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", safeHandler(listHandler))
+	mux.HandleFunc("/view", safeHandler(viewHandler))
+	mux.HandleFunc("/upload", safeHandler(uploadHandler))
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err.Error())
 	}
 }
